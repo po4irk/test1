74c5d7372b73ad468da0f7167f81d76a76757dc3
po4irk <Psuboch@yandex.ru>
2025-08-31 14:28:23 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,7 @@
-print("hello world")
-input("Press Enter to exit...")  
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Пустое окно")
+root.geometry("400x300")
+root.configure(bg="white")
+root.mainloop()
